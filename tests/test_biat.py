7a44e6ccb1ad85a42211107265b4c51eccaf6b34
Herman Zhao <hermanzhaozzzz@gmail.com>
2022-10-12 16:44:56 +0800

    init

--- tests/test_biat.py
+++ tests/test_biat.py
@@ -1,5 +1,13 @@
-from biat import __version__
+import biat
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert biat.__version__ == '0.1.0'
+
+
+def main():
+    test_version()
+
+
+if __name__ == '__main__':
+    main()
