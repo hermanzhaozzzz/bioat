5145fa904ba0dfe26bfe5484bcce9b7421471e38
Herman Zhao <hermanzhaozzzz@gmail.com>
2022-10-14 20:08:44 +0800

    v0.1.1.2

---
+++ scripts/set_env.sh
@@ -0,0 +1,4 @@
+export https_proxy=http://127.0.0.1:6152;
+export http_proxy=http://127.0.0.1:6152;
+export all_proxy=socks5://127.0.0.1:6153;
+export PYTHONPATH=/Users/zhaohuanan/PycharmProjects/BioinformaticAnalysisTools/src;
