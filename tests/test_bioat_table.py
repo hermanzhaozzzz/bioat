380855236d3633758c48d74d282e313bffebec2e
Herman Zhao <hermanzhaozzzz@gmail.com>
2022-10-18 11:32:34 +0800

    v0.1.1.3 add table/merge_table

---
+++ tests/test_bioat_table.py
@@ -0,0 +1,7 @@
+from fire import Fire
+from bioat.table.merge_table import main as run_merge_table
+
+
+# def test_merge_table():
+    # TODO 暂时不知道测试方案
+#     Fire(run_merge_table, command="from_tsv", args=["--help"])
