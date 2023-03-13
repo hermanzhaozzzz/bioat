c5b43aede8546fe9f911882e4f140168b972030a
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-03-13 23:23:37 +0800

    refactor bioat3

--- src/bioat/version.py
+++ src/bioat/version.py
@@ -1,5 +1,5 @@
 __upgrade_date__ = '2022-10-18'
-__version__ = '0.1.1.5'
+__version__ = '0.1.2.0'
 __author__ = 'Hua-nan ZHAO @ Tsinghua University'
 __email__ = 'hermanzhaozzzz@gmail.com'
 __doc_format__ = "restructuredtext"
