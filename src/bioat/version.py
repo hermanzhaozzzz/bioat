552ecc23f6f8bbc2ab262d5bfcf012be00a29cb9
hermanzhaozzzz <hermanzhaozzzz@gmail.com>
2023-04-06 20:05:36 +0800

    get version from toml

--- src/bioat/version.py
+++ src/bioat/version.py
@@ -1,5 +1,8 @@
+with open('../../pyproject.toml', 'rt') as f:
+    __version__ = [i.rstrip().split('= ')[-1]
+                   for i in f.readlines() if 'version =' in i][0].replace('"', '')
+
 __upgrade_date__ = '2023-03-31'
-__version__ = '0.1.4.1'
 __author__ = 'Hua-nan ZHAO @ Tsinghua University'
 __email__ = 'hermanzhaozzzz@gmail.com'
 __doc_format__ = "restructuredtext"
