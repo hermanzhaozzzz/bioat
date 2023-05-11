113992d71e6085a2ec5d57d315d0544746904ced
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-05-11 22:58:59 +0800

    v0.1.5.4

--- src/bioat/version.py
+++ src/bioat/version.py
@@ -6,7 +6,7 @@ import os
 project_toml: str = os.path.join(bioat.__path__[0], 'version.py')
 sec = os.path.getmtime(project_toml)
 __upgrade_date__ = datetime.date.fromtimestamp(sec)
-__version__ = "0.1.5.3"
+__version__ = "0.1.5.4"
 
 __author__ = 'Hua-nan ZHAO @ Tsinghua University'
 __email__ = 'hermanzhaozzzz@gmail.com'
