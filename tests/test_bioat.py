128a26c9ca8fc41fa5dc7334c5f4e3eb542f43d8
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-08-04 23:04:46 +0800

    0.2.7 add module libpandas

--- tests/test_bioat.py
+++ tests/test_bioat.py
@@ -1,6 +1,6 @@
 import bioat
 
 def test_version_info():
-    assert bioat.__version__ == '0.2.5'
+    assert bioat.__version__ == '0.2.7'
     assert bioat.__author__ == 'Hua-nan ZHAO @ Tsinghua University'
     assert bioat.__doc_format__ == "restructuredtext"
