5f658bedbd86cfb948e1a0b28a6ae64b26b7c34f
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-08-15 16:33:27 +0800

    0.4.1

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bioat'
+copyright = '2023, Hua-nan Herman ZHAO'
+author = 'Hua-nan Herman ZHAO'
+release = '0.4.1'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+extensions = [
+        'recommonmark',
+        'sphinx_markdown_tables',
+        'sphinxemoji.sphinxemoji',
+]
+
+# -- Options for HTML output ------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# The master toctree document.
+master_doc = 'index'
