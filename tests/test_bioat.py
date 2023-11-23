24765797ca268ba0b58643186daef775bda45444
hermanzhaozzzz <hermanzhaozzzz@gmail.com>
2023-11-24 02:36:56 +0800

    fix tests

--- tests/test_bioat.py
+++ tests/test_bioat.py
@@ -2,5 +2,5 @@ import bioat
 
 def test_version_info():
     assert bioat.__version__ == "0.4.11"
-    assert bioat.__author__ == 'Hua-nan ZHAO @ Tsinghua University'
+    assert bioat.__author__ == 'Huanan Herman ZHAO @ Tsinghua University'
     assert bioat.__doc_format__ == "restructuredtext"
