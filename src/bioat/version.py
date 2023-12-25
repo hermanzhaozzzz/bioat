70711c4ad592d731a4a8124ac9fa2d1d29a2b2f5
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-12-25 22:42:24 +0800

    fix | type error, no longer support 3.9

--- src/bioat/version.py
+++ src/bioat/version.py
@@ -1,11 +1,12 @@
 import bioat
+
 import datetime
 import os
 
 project_toml: str = os.path.join(bioat.__path__[0], "version.py")
 sec = os.path.getmtime(project_toml)
 __upgrade_date__ = datetime.date.fromtimestamp(sec)
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 __author__ = "Huanan Herman ZHAO @ Tsinghua University"
 __email__ = "hermanzhaozzzz AT gmail.com"
