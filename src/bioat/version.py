dc54d6e9f38a27084d1682a3fcb99f6dfaf1261a
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-12-25 22:26:07 +0800

    finish the development for cas_finder and cas13_finder

--- src/bioat/version.py
+++ src/bioat/version.py
@@ -2,13 +2,13 @@ import bioat
 import datetime
 import os
 
-project_toml: str = os.path.join(bioat.__path__[0], 'version.py')
+project_toml: str = os.path.join(bioat.__path__[0], "version.py")
 sec = os.path.getmtime(project_toml)
 __upgrade_date__ = datetime.date.fromtimestamp(sec)
-__version__ = "0.6.2"
+__version__ = "0.7.1"
 
-__author__ = 'Huanan Herman ZHAO @ Tsinghua University'
-__email__ = 'hermanzhaozzzz AT gmail.com'
+__author__ = "Huanan Herman ZHAO @ Tsinghua University"
+__email__ = "hermanzhaozzzz AT gmail.com"
 __doc_format__ = "restructuredtext"
 __doc_address__ = "https://github.com/hermanzhaozzzz/bioat/tree/master/docs"
 __issue_address__ = "https://github.com/hermanzhaozzzz/bioat/issues"
