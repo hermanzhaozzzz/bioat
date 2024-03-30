a35424dade9ff7e27db1d15fc85e316de5c9f40a
Herman Zhao <hermanzhaozzzz@gmail.com>
2024-03-30 18:53:34 +0800

    update libpyplot

--- src/bioat/lib/libplot.py
+++ src/bioat/lib/libplot.py
@@ -1,8 +1,30 @@
+import sys
 import math
-from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
-def plot_colortable(colors, *, ncols=4, sort_colors=True, labels=None):
+from matplotlib.patches import Rectangle
+from bioat.logger import get_logger
 
+__all__ = ['init_matplotlib']
+__module_name__ = 'bioat.lib.libpandas'
+
+
+def init_matplotlib(log_level='INFO'):
+    logger = get_logger(level=log_level, module_name=__module_name__, func_name=sys._getframe().f_code.co_name)
+    logger.info('Initializing matplotlib')
+    logger.debug('ref: https://matplotlib.org/stable/api/style_api.html')
+    logger.debug("set: plt.style.use('ggplot')  # use ggplot style")
+    plt.style.use('ggplot')
+
+    logger.debug('ref: https://matplotlib.org/stable/api/matplotlib_configuration_api.html#matplotlib.rcParams')
+    logger.info("set: plt.rcParams['pdf.use14corefonts'] = True  # trigger core fonts for PDF backend")
+    plt.rcParams['pdf.use14corefonts'] = True
+    logger.info("set: plt.rcParams['ps.useafm'] = True  # trigger core fonts for PS backend")
+    plt.rcParams['ps.useafm'] = True
+    logger.info("set: plt.rcParams['svg.fonttype'] = 'none'  # change 'path' to 'none' (use font)for SVG backend")
+    plt.rcParams['svg.fonttype'] = 'none'
+
+
+def plot_colortable(colors, *, ncols=4, sort_colors=True, labels=None):
     cell_width = 212
     cell_height = 22
     swatch_width = 48
@@ -48,7 +70,10 @@ def plot_colortable(colors, *, ncols=4, sort_colors=True, labels=None):
 
     return fig
 
+
 if __name__ == '__main__':
+    # test for init_matplotlib
+    init_matplotlib(log_level='debug')
     # %%% test func plot_colortable
     colors = ['#64C1E8',
               '#80CED7',
